590771610e4aa4c013e208a66a47ba8cfed6e14a
Kyle Peacock <kyle.peacock@urbanelectric.com>
2019-07-19 12:22:28 -0400

    fix weird subnet issue

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "brewery-app-subnet" {
   vpc_id = "${var.brewery_app_vpc_id}"
-  cidr_block = "172.31.100.0/20"
+  cidr_block = "172.31.96.0/20"
 
   tags = {
     Name = "brewery-app-subnet"
